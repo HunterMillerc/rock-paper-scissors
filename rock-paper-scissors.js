1af6c069fc43eb36ef507cf8348a29fc1b737a10
HunterMillerc <miller.hunterc@gmail.com>
2019-11-14 00:41:09 -0600

    Update rock-paper-scissors.js
    GOT IT TO WORK OHHH YEA

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,317 +1,234 @@
-//Add variables for elements needed in game
-var playerChoices = document.querySelectorAll('.choices');
-var playerChoiceBackground = document.getElementById('playerChoiceIcon');
-var descPlayerChoice = document.getElementById('playerSelection');
-var computerChoiceBackground = document.getElementById('computerChoiceIcon');
-var bottomText = document.getElementById('bottomText');
-var descComputerChoice = document.getElementById('computerSelection');
-var updatePlayerScore = document.getElementById('playerScore');
-var updateComputerScore = document.getElementById('computerScore');
-var playerAddOne = document.querySelector('#playerAddOneText');
-var computerAddOne = document.querySelector('#computerAddOneText');
-//Hides playAgain selections at start
-var playAgainSelections = document.querySelectorAll('.playAgain');
-function hidePlayAgainSelections()
-{
-    [...playAgainSelections].forEach(function(playAgainSelections)
-    {
-        playAgainSelections.style.visibility = 'hidden';
-    });
+//Variables for choice icon backgrounds and text display for choices on top of page
+let choiceIcons = document.getElementsByClassName('choiceIcon');
+let playerChoiceIcon = document.getElementById('playerChoiceIcon');
+let playerChoiceText = document.getElementById('playerSelection');
+let computerChoiceIcon = document.getElementById('computerChoiceIcon');
+let computerChoiceText = document.getElementById('computerSelection');
+
+//Variables for choices in middle of page and updated scores
+let playerChoices = document.getElementsByClassName('choices');
+let playerScoreText = document.getElementById('playerScore');
+let computerScoreText = document.getElementById('computerScore');
+
+//Variables for bottom +1/Tie text, new game buttons, bottom text
+let playerAddOneText = document.getElementById('playerAddOneText');
+let computerAddOneText = document.getElementById('computerAddOneText');
+let bothAddOneText = document.getElementsByClassName('addOneText');
+let playAgainChoices = document.getElementsByClassName('playAgain');
+let bottomText = document.getElementById('bottomText');
+
+//Hide choiceIcons at beginning
+[...choiceIcons].forEach(choiceIcon => choiceIcon.classList.remove("iconPaper", "iconScissors", "iconRock"));
+
+//Hide playAgain choices & show playAgain choices
+function hidePlayAgainChoices(){
+    for(let i = 0; i < playAgainChoices.length; i++){
+        playAgainChoices[i].style.visibility = 'hidden'
+    }
 }
 
-//Initiate both scores to = 0
-var playerScore = 0;
-var computerScore = 0;
-
-//Loops through rock, paper, scissor icons to make mouse pointer into 'pointer'
-//Does same thing for playAgainSelections at end of game when playAgainSelections shown
-
-
-[...playerChoices].forEach(function(playerChoiceMouseOvers)
-{
-    playerChoiceMouseOvers.addEventListener('mouseover', function()
-    {
-        playerChoiceMouseOvers.style.cursor = 'pointer';
-    });
-});
-
-[...playAgainSelections].forEach(function(playAgainSelections)
-{
-    playAgainSelections.addEventListener('mouseover', function()
-    {
-        playAgainSelections.style.cursor = 'pointer';
-    });
-});
-
-//Show player choices at start of game
-function showPlayerChoices()
-{
-    playerChoices.forEach(function(playerChoices)
-    {
-        playerChoices.style.visibility = 'visible';
-    });
+function showPlayAgainChoices(){
+    for(let i = 0; i < playAgainChoices.length; i++){
+        playAgainChoices[i].style.visibility = 'visible';
+        playAgainChoices[i].style.cursor = 'pointer';
+        if (playerScore > computerScore){
+            bottomText.textContent = "You won! Play again?";
+        }
+        else{
+            bottomText.textContent = "The computer won! Try again?";
+        }
+    }
+    for(let i = 0; i < bothAddOneText.length; i++){
+        bothAddOneText[i].removeEventListener('transitionend', showPlayAgainChoices);
+    }
+    for(let i = 0; i < playerChoices.length; i++){
+        playerChoices[i].style.visibility = 'hidden';
+    }
 }
 
-//play Game
-
-function playGame()
-{
-    playerChoiceBackground.classList.remove('iconPaper', 'iconScissors', 'iconRock');
-    computerChoiceBackground.classList.remove('iconPaper', 'iconScissors', 'iconRock');
-    bottomText.textContent = "Please choose an option: Rock, Paper, or Scissors";
-    hidePlayAgainSelections();
-    showPlayerChoices();
-    playerChoices.forEach((playerChoice) =>
-    {
-        playerChoice.addEventListener('click', () =>
-        {
-            var playerGuess = playerChoice.id;
-            if (playerGuess == 'paper')
-            {
-                playerChoiceBackground.classList.add('iconPaper');
-                descPlayerChoice.textContent = "Paper";
-            }
-            else if (playerGuess == 'scissors')
-            {
-                playerChoiceBackground.classList.add('iconScissors');
-                descPlayerChoice.textContent = "Scissors";
-            }
-            else if (playerGuess == 'rock')
-            {
-                playerChoiceBackground.classList.add('iconRock');
-                descPlayerChoice.textContent = "Rock";
-            }
-            computerGuess = returnComputerGuess();
-            var result = compareGuesses(playerGuess, computerGuess);
-            updateScores(result);
-            if (playerScore == 3)
-            {
-                gameEnd();
-                playerWon();
-            }
-            else if (computerScore == 3)
-            {
-                gameEnd();
-                computerWon();
-            }
-        });
-    });
+//Show playerChoices
+function showPlayerChoices(){
+    for(let i = 0; i < playerChoices.length; i++){
+        playerChoices[i].style.visibility = 'visible';
+    }
 }
 
-//Disable player choice divs when game is over
-
-function playerWon()
-{
-    [...addOneText].forEach(function(addOneText)
-    {
-        addOneText.addEventListener('transitionend', () =>
-        {
-            playerChoices.forEach(function(playerChoices)
-            {
-                playerChoices.style.visibility = 'hidden';
-                bottomText.textContent = "You won! Play again?";
-            });
-        });
-    });
+//Make cursor for playerChoices pointer
+for(let i = 0; i < playerChoices.length; i++){
+    playerChoices[i].style.cursor = "pointer";
 }
 
-function computerWon()
+//Random computer selection
+function computerGuess() 
 {
-    [...addOneText].forEach(function(addOneText)
-    {
-        addOneText.addEventListener('transitionend', () =>
-        {
-            playerChoices.forEach(function(playerChoices)
-            {
-                playerChoices.style.visibility = 'hidden';
-                bottomText.textContent = "The computer won! Try again?";
-            });
-        });
-    });
-}
-
-
-//Provides random selection for computer and introduces resulting image to 
-//computer choice icon on top right of page
-//Also provides text below icon depending on choice
-
-function returnComputerGuess() 
-{
-    var computerInput = ["rock", "paper", "scissors"];
-    var computerInputGuess = computerInput[Math.floor(Math.random() * computerInput.length)];
-    if (computerInputGuess === "paper")
-    {
-        computerChoiceBackground.classList.add('iconPaper');
-        descComputerChoice.textContent = "Paper";
+    let computerInput = ["rock", "paper", "scissors"];
+    let computerInputGuess = computerInput[Math.floor(Math.random() * computerInput.length)];
+    if (computerInputGuess === "paper"){
+        computerChoiceIcon.classList.add('iconPaper');
+        computerChoiceText.textContent = "Paper";
         return computerInputGuess = "paper";
     }
-    else if (computerInputGuess === "scissors")
-    {
-        computerChoiceBackground.classList.add('iconScissors');
-        descComputerChoice.textContent = "Scissors";
+    else if (computerInputGuess === "scissors"){
+        computerChoiceIcon.classList.add('iconScissors');
+        computerChoiceText.textContent = "Scissors";
         return computerInputGuess = "scissors";
     }
-    else if (computerInputGuess === "rock")
-    {
-        computerChoiceBackground.classList.add('iconRock');
-        descComputerChoice.textContent = "Rock";
+    else if (computerInputGuess === "rock"){
+        computerChoiceIcon.classList.add('iconRock');
+        computerChoiceText.textContent = "Rock";
         return computerInputGuess = "rock";
     }
 }
 
+//Clear icons on top of page
+function clearIcons(){
+    playerChoiceIcon.classList.remove('iconPaper', 'iconScissors', 'iconRock');
+    playerChoiceText.textContent = "";
+    computerChoiceIcon.classList.remove('iconPaper', 'iconScissors', 'iconRock');
+    computerChoiceText.textContent = "";
+}
+
 //Compare the guesses
-function compareGuesses(playerGuess, computerGuess)
-{
-    if (playerGuess === computerGuess)
-    {
+function compareGuesses(playerSelection, computerSelection){
+    if (playerSelection === computerSelection){
         return 0;
     }
     else if 
     (
-    (playerGuess === 'rock' && computerGuess === 'scissors')
-                                ||
-    (playerGuess === 'paper' && computerGuess === 'rock')
-                                ||
-    (playerGuess === 'scissors' && computerGuess === 'paper')
-    )
-    {
+        (playerSelection === 'rock' && computerSelection === 'scissors')
+                                    ||
+        (playerSelection === 'paper' && computerSelection === 'rock')
+                                    ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
         return 1;
     }
-    else 
-    {
+    else {
         return 2;
     }
 }
 
-//Update scores and perform transition for +1 or tie at bottom of page, then remove when transition done
-
-function updateScores(result)
-{
-    if(result === 1)
-    {
+//Update player scores
+function updateScores(result){
+    if(result === 1){
         playerScore ++;
-        updatePlayerScore.textContent = playerScore;
-        updateComputerScore.textContent = computerScore;
-        playerAddOne.textContent = "+1";
+        playerScoreText.textContent = playerScore;
+        computerScoreText.textContent = computerScore;
+        playerAddOneText.textContent = "+1";
         transform();
-        removeText();
+        for(let i = 0; i < bothAddOneText.length; i++){
+            bothAddOneText[i].addEventListener('transitionend', removeText);
+        }
     }
     if (result === 2)
     {
         computerScore ++;
-        updateComputerScore.textContent = computerScore;
-        updatePlayerScore.textContent = playerScore;
-        computerAddOne.textContent = "+1";
+        computerScoreText.textContent = computerScore;
+        playerScoreText.textContent = playerScore;
+        computerAddOneText.textContent = "+1";
         transform();
-        removeText();
+        for(let i = 0; i < bothAddOneText.length; i++){
+            bothAddOneText[i].addEventListener('transitionend', removeText);
+        }
     }
     if (result === 0)
     {
-        updateComputerScore.textContent = computerScore;
-        updatePlayerScore.textContent = playerScore;
+        computerScoreText.textContent = computerScore;
+        playerScoreText.textContent = playerScore;
         sayTie();
         transform();
-        removeText();
+        for(let i = 0; i < bothAddOneText.length; i++){
+            bothAddOneText[i].addEventListener('transitionend', removeText);
+        }
     }
 }
 
-//Add +1 or tie on bottom divs functions
-var addOneText = document.getElementsByClassName('addOneText');
-
-function sayTie()
-{
-    [...addOneText].forEach(function(addOneText)
-    {
-        addOneText.textContent = "Tie!";
-    });
+//Say Tie! on both addOneText
+function sayTie(){
+    for(let i = 0; i < bothAddOneText.length; i++){
+        bothAddOneText[i].textContent = "Tie!";
+    }
 }
 
-function transform()
-{
-        [...addOneText].forEach(function(addOneText)
-        {
-            addOneText.classList.add('scale');
-            playerChoices.forEach(function(playerChoices)
-            {
-                playerChoices.classList.add('wait');
-            });
-        });
+//Add scale to bothAddOneText, make player wait until animation done
+function transform(){
+    for(let i = 0; i < bothAddOneText.length; i++){
+        bothAddOneText[i].classList.add('scale');
+    }
+    for(let i = 0; i < playerChoices.length; i++){
+        playerChoices[i].classList.add('wait');
+    }
 }
 
-function removeText()
-{
-    [...addOneText].forEach(function(addOneText)
-    {
-        addOneText.addEventListener('transitionend', () => 
-        {
-            playerChoices.forEach(function(playerChoices)
-            {
-                playerChoices.classList.remove('wait');
-            });
-            addOneText.textContent = '';
-            removeScale();
-            clearIcons();
-        });
-    });
+function removeText(){
+    for(let i = 0; i < bothAddOneText.length; i++){
+        bothAddOneText[i].classList.remove('scale');
+        bothAddOneText[i].textContent = "";
+        bothAddOneText[i].removeEventListener('transitionend', removeText);
+    }
+    for(let i = 0; i < playerChoices.length; i++){
+        playerChoices[i].classList.remove('wait');
+    }
+    clearIcons();
 }
+window.addEventListener('load', game);
+//Start game
+let playerScore = 0;
+let computerScore = 0;
+function game(){
+    bottomText.textContent = "Please pick a choice: Rock, Paper, or Scissors";
+    hidePlayAgainChoices();
+    showPlayerChoices();
+    for(let i = 0; i < playerChoices.length; i++){
+        playerChoices[i].addEventListener('click', playerClickChoice);
+    }
 
-function removeScale()
-{
-    [...addOneText].forEach(function(addOneText)
-    {
-       addOneText.classList.remove('scale'); 
-    });
+    function playerClickChoice(e){
+        let playerSelection = e.target.id;
+        if (playerSelection == 'paper') {
+            playerChoiceIcon.classList.add('iconPaper');
+            playerChoiceText.textContent = "Paper";
+        }
+        else if (playerSelection == 'scissors'){
+            playerChoiceIcon.classList.add('iconScissors');
+            playerChoiceText.textContent = "Scissors";
+        }
+        else if (playerSelection == 'rock'){
+            playerChoiceIcon.classList.add('iconRock');
+            playerChoiceText.textContent = "Rock";
+        }
+        let computerSelection = computerGuess();
+        let result = compareGuesses(playerSelection, computerSelection)
+        updateScores(result);
+        if(playerScore == 3 || computerScore == 3){
+            playerScore = 0;
+            computerScore = 0;
+            for(let i = 0; i < playerChoices.length; i++){
+                playerChoices[i].removeEventListener('click', playerClickChoice);
+            }
+            for(let i = 0; i < bothAddOneText.length; i++){
+                bothAddOneText[i].addEventListener('transitionend', showPlayAgainChoices);
+            }
+            for(let i = 0; i < playAgainChoices.length; i++){
+                playAgainChoices[i].addEventListener('click', playAgainClick)
+            }
+            function playAgainClick(e){
+                let playAgainClickChoice = e.target.id;
+                if(playAgainClickChoice == "noPlayAgain"){
+                    for(let i = 0; i < playAgainChoices.length; i++){
+                        playAgainChoices[i].style.visibility = 'hidden';
+                    }
+                    bottomText.textContent = "Enjoy the scenery!";
+                }
+                else if (playAgainClickChoice == "yesPlayAgain"){
+                    for(let i = 0; i < playAgainChoices.length; i++){
+                        playAgainChoices[i].removeEventListener('click', playAgainClick);
+                    }
+                    playerScoreText.textContent = "";
+                    computerScoreText.textContent = "";
+                    game();
+                }
+            }
+        }
+    }
 }
-
-//Get rid of icons shown on top of page for choices
-function clearIcons()
-{
-    playerChoiceBackground.classList.remove('iconPaper', 'iconScissors', 'iconRock');
-    descPlayerChoice.textContent = "";
-    computerChoiceBackground.classList.remove('iconPaper', 'iconScissors', 'iconRock');
-    descComputerChoice.textContent = "";
-}
-
-//Enable gameEnd occurence. Allow user to click yes to play again or no to not play again
-//Currently, resetting scores shows each score as 'undefined' when play again yes performed
-var yesPlayAgain = document.getElementById('yesPlayAgain');
-var noPlayAgain = document.getElementById('noPlayAgain');
-function gameEnd()
-{
-    showPlayAgainSelections();
-    yesPlayAgain.addEventListener('click', () =>
-    {
-        resetScores();
-        playGame();
-    });
-    noPlayAgain.addEventListener('click', () =>
-    {
-        hidePlayAgainSelections();
-        bottomText.textContent = "Enjoy the scenic layout of the webpage!";
-    });
-}
-
-//Showing the divs for thumbs up or down to play again or not
-function showPlayAgainSelections()
-{
-    [...addOneText].forEach(function(addOneText)
-    {
-        addOneText.addEventListener('transitionend', () =>
-        {
-            [...playAgainSelections].forEach(function(playAgainSelections)
-            {
-                playAgainSelections.style.visibility = 'visible';
-            });
-        });
-    });
-}
-
-//Reset scores to 0
-function resetScores()
-{
-    playerScore = 0;
-    computerScore = 0;
-    updateComputerScore.textContent = 0;
-    updatePlayerScore.textContent = 0;
-}
-playGame();
